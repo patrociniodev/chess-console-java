a7842973791a59dae5df8e43e29cb374625c87e3
= <patrociniodev@gmail.com>
2026-02-20 16:27:12 -0300

    Create and start to implement Board and Piece classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,11 @@
 package application;
 
+import board.Board;
 import board.Position;
 
 public class Program {
 
     public static void main(String[] args) {
-
-        Position p = new Position(2, 4);
-        System.out.println(p.toString());
+        Board board = new Board(8, 8);
     }
 }
