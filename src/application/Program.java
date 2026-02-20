7ef78c481c9fcc2d0484d0fa58f59895d23a1000
= <patrociniodev@gmail.com>
2026-02-20 16:15:39 -0300

    Create class Position

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import board.Position;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Position p = new Position(2, 4);
+        System.out.println(p.toString());
+    }
+}
