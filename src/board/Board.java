28714b925abb8b3b79c1fa0a7624fe35189c87c4
= <patrociniodev@gmail.com>
2026-02-22 13:23:13 -0300

    Create BoardException on board layer and implements defensive programming

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,5 +1,7 @@
 package board;
 
+import board.exceptions.BoardException;
+
 public class Board {
 
     private int rows;
@@ -7,6 +9,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -21,15 +26,39 @@ public class Board {
     }
 
     public Piece piece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("There is not a piece on the given position");
+        }
         return pieces[row][column];
     }
 
-    public Piece piece(Position pos) {
-        return pieces[pos.getRow()][pos.getColumn()];
+    public Piece piece(Position p) {
+        if (!positionExists(p)) {
+            throw new BoardException("There is not a piece on the given position");
+        }
+        return pieces[p.getRow()][p.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece on position: " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    public boolean thereIsAPiece(Position p) {
+        if (!positionExists(p)) {
+            throw new BoardException("Position is not on the board");
+        }
+        return piece(p) != null;
+    }
+
+    public boolean positionExists(Position p) {
+        return positionExists(p.getRow(), p.getColumn());
+    }
+
+    private boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
     }
 }
---
+++ src/board/exceptions/BoardException.java
@@ -0,0 +1,8 @@
+package board.exceptions;
+
+public class BoardException extends RuntimeException{
+
+    public BoardException(String message) {
+        super(message);
+    }
+}
